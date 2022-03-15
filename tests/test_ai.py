58cd4fda02e239aa5ac44d2fb08efe3245af66b2
Stas Magdych <mustas@pm.me>
2022-03-15 16:12:20 +0100

    added AI tests to hopefully pass

--- tests/test_ai.py
+++ tests/test_ai.py
@@ -9,14 +9,22 @@ from .test_mancala import INITIAL_BOARD, STEALER_BOARD
 
 
 @pytest.mark.parametrize("board", [INITIAL_BOARD, STEALER_BOARD])
-def test(board):
+@pytest.mark.parametrize("heuristic", [H1, H2, H3, H4])
+@pytest.mark.parametrize("max_depth", [1, 2, 3, 4])
+
+def test_MiniMax_and_AlphaBeta(board, heuristic, max_depth):
 
     test_game = Mancala(board)
 
-    MiniMax = MiniMaxPlayer(test_game, 2, H3)
-    AlphaBeta = AlphaBetaPlayer(test_game, 2, H3)
+    MiniMax = MiniMaxPlayer(test_game, max_depth, heuristic)
+    AlphaBeta = AlphaBetaPlayer(test_game, max_depth, heuristic)
 
     mini_max_move = MiniMax.think(test_game.board, 0)
     alpha_beta_move = AlphaBeta.think(test_game.board, 0)
 
+    assert mini_max_move == alpha_beta_move
+
+    mini_max_move = MiniMax.think(test_game.board, 1)
+    alpha_beta_move = AlphaBeta.think(test_game.board, 1)
+    
     assert mini_max_move == alpha_beta_move
